ec8b2530382313144812e984cba1db2631238e44
ghizlane <boufeljaghizlane@gmail.com>
2025-06-06 05:56:16 +0100

    init commit

---
+++ IdeaProjects/Gestion_Bibliotheque_PFE/src/main/java/org/ghizlane/gestion_bibliotheque_pfe/models/Livre.java
@@ -0,0 +1,58 @@
+package org.ghizlane.gestion_bibliotheque_pfe.models;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "livre")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_livre;
+
+    private String titre;
+    private String auteur;
+    private int annee_edition;
+    private String imagePath;
+
+    @Transient
+    private MultipartFile imageFile;
+
+
+
+
+
+    private int nbrExempDisponibles;
+
+    @ManyToOne
+    @JoinColumn(name = "categorie_id", referencedColumnName = "id_categorie")
+    @JsonIgnoreProperties("livres")
+    private Categorie categorie;
+
+
+    @OneToMany(mappedBy = "livre", cascade = CascadeType.ALL)
+    private List<Emprunt> emprunts;
+
+    @OneToMany(mappedBy = "livre", cascade = CascadeType.ALL)
+    private List<Reservation> reservations;
+
+    @ManyToMany(mappedBy = "livres")
+    @JsonIgnoreProperties("livres")
+    private List<Promotion> promotions = new ArrayList<>();
+
+
+}
